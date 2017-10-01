8da62c304dfe531d8f0d09674076854aa191f441
YarPodl <yar.podlesnov@yandex.ru>
2017-10-01 10:51:31 +0300

    начало

---
+++ src/digSearch/digSearch.java
@@ -0,0 +1,34 @@
+package digSearch;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class digSearch {
+
+
+    /*
+    private Map<Character, Map> tree;
+
+    public digSearch() {
+        tree = new HashMap<>();
+    }
+
+    public String add(String value){
+        if (!value.isEmpty()){
+            return "Пустое слово";
+        }
+
+        for (int i = 0; i < value.length(); i++) {
+            Map current = tree.get(value.charAt(i));
+            if (current == null){
+                tree.put(value.charAt(i), new HashMap());
+            }
+            tree.put(value.charAt(i), );
+        }
+    }
+
+    */
+
+
+    
+}
