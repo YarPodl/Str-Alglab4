b80063198d5b2d1088cbeaaa96a23320b11f02d3
YarPodl <yar.podlesnov@yandex.ru>
2017-10-22 23:14:17 +0300

    Готово

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,19 +1,42 @@
 package Main;
 import digSearch2.DigSearch;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
         DigSearch digSearch = new DigSearch();
-        digSearch.add("qwer");
-        digSearch.add("tuty");
-        digSearch.add("luy");
-        digSearch.add("luaqqe");
-        System.out.println(digSearch.search(""));
-        System.out.println(digSearch.search("qw"));
-        System.out.println(digSearch.search("qwer"));
-        System.out.println(digSearch.search("qwert"));
-        System.out.println(digSearch.search("luy"));
-        System.out.println(digSearch.search("luaqqe"));
-        System.out.println(digSearch.search("qwert"));
+        MyForm myForm = new MyForm();
+        myForm.revalidate();
+        myForm.buttonSearch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (myForm.textField.getText().isEmpty()){
+                    myForm.label.setText("Поле ввода пусто");
+                } else {
+                    if (digSearch.search(myForm.textField.getText())) {
+                        myForm.label.setText("Слово найдено");
+                    } else {
+                        myForm.label.setText("Слово отсутствует");
+                    }
+                }
+            }
+        });
+        myForm.buttonAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (myForm.textField.getText().isEmpty()){
+                    myForm.label.setText("Поле ввода пусто");
+                } else {
+                    if (digSearch.add(myForm.textField.getText())) {
+                        myForm.label.setText("Слово добавлено");
+                        myForm.listModel.addElement(myForm.textField.getText());
+                    } else {
+                        myForm.label.setText("Слово уже имеется");
+                    }
+                }
+            }
+        });
     }
 }
--- src/Main/MyForm.java
+++ src/Main/MyForm.java
@@ -1,7 +1,65 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyForm extends JFrame {
 
+    public JButton buttonSearch;
+    public JButton buttonAdd;
+    public JTextField textField;
+    public JLabel label;
+    public JList listBox;
+    public JPanel panel;
+    public DefaultListModel listModel;
+
+    public MyForm() {
+        super();
+        //UIManager.getSystemLookAndFeelClassName ();
+        setMinimumSize(new Dimension(300, 300));
+        setSize(450, 300);
+        setVisible(true);
+        setTitle("Лабораторня работа №4. Цифровой поиск");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+
+        setLayout(new BorderLayout());
+
+        listModel = new DefaultListModel();
+        listBox = new JList(listModel);
+        getContentPane().add(listBox, BorderLayout.CENTER);
+
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        getContentPane().add(panel, BorderLayout.EAST);
+        panel.setPreferredSize(new Dimension(150, 300));
+
+        buttonAdd = new JButton("Добавить");
+        buttonAdd.setPreferredSize(new Dimension(150, 25));
+
+        buttonSearch = new JButton("Найти");
+        buttonSearch.setPreferredSize(new Dimension(100, 25));
+
+        textField = new JTextField();
+        textField.setMaximumSize(new Dimension(300, 25));
+
+        label = new JLabel("");
+
+
+        panel.add(Box.createVerticalStrut(20));
+        panel.add(new JLabel("Введите слово"));
+        panel.add(textField);
+        panel.add(Box.createVerticalStrut(20));
+        panel.add(buttonAdd);
+        panel.add(Box.createVerticalStrut(20));
+        panel.add(buttonSearch);
+        panel.add(Box.createVerticalStrut(20));
+        panel.add(label);
+
+
+    }
 }
